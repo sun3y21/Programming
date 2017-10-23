985477989c37f58c22df3b6af6e4437539033be0
Sunny <sun3ysaini@gmail.com>
2017-10-23 20:39:05 +0530

    Start

---
+++ Stack/Stack/Question1.cpp
@@ -0,0 +1,9 @@
+//
+//  Question1.cpp
+//  Stack
+//
+//  Created by Sunny Saini on 23/10/17.
+//  Copyright © 2017 Sunny Saini. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Stack/Stack/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "Question1.cpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
